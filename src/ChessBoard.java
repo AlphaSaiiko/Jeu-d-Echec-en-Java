ef05a7f2406070fa29b6d9c12db806223e605a61
Antoine Lechasles <antoine.lechasles@etu.univ-lehavre.fr>
2024-04-15 10:46:11 +0200

    mise a jour panel

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,68 +1,75 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class ChessBoard
+
+public class ChessBoard 
 {
-	public static void main(String[] args)
+	private JFrame frame;
+	private PiecePanel piecePanel;
+	private JPanel boardPanel;
+	private JButton[][] boardSquares;
+
+	public ChessBoard()
 	{
-		JFrame frame = new JFrame("Chess Board");
+		frame = new JFrame("Chess Board");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(800, 800); // Set the size of the frame
 
-		JPanel boardPanel = new JPanel(new GridLayout(8, 8));
+		// Create the PiecePanel
+		piecePanel = new PiecePanel();
+		frame.add(piecePanel, BorderLayout.CENTER);
+
+		boardPanel = new JPanel(new GridLayout(8, 8));
 		frame.add(boardPanel, BorderLayout.CENTER);
 
 		Color colorBlack = Color.BLACK;
 		Color colorWhite = Color.WHITE;
-		JButton[][] boardSquares = new JButton[8][8];
+		boardSquares = new JButton[8][8];
 
 		String[] pieces = { "R", "N", "B", "Q", "K", "B", "N", "R" }; // Letters
 																		// representing
 																		// the
 																		// pieces
-																		
+
 		for (int i = 0; i < 8; i++)
 		{
 			for (int j = 0; j < 8; j++)
 			{
-				JButton button = new JButton();
-				button.setPreferredSize(new Dimension(100, 100)); // Set the
-																	// size of
-																	// the
-																	// button
+				boardSquares[i][j] = new JButton();
+				boardPanel.add(boardSquares[i][j]);
+
 				if ((i + j) % 2 == 0)
 				{
-					button.setBackground(colorWhite);
+					boardSquares[i][j].setBackground(colorWhite);
 				}
 				else
 				{
-					button.setBackground(colorBlack);
+					boardSquares[i][j].setBackground(colorBlack);
 				}
+
 				if (i == 0)
 				{
-					button.setText(pieces[j]); // Set the text of the button to
-												// the corresponding piece
+					boardSquares[i][j].setText(pieces[j]);
 				}
 				else if (i == 1)
 				{
-					button.setText("P"); // Set the text of the button to "P"
-											// for pawns
+					boardSquares[i][j].setText("P");
 				}
 				else if (i == 6)
 				{
-					button.setText("P"); // Set the text of the button to "P"
-											// for pawns
+					boardSquares[i][j].setText("p");
 				}
 				else if (i == 7)
 				{
-					button.setText(pieces[j]); // Set the text of the button to
-												// the corresponding piece
+					boardSquares[i][j].setText(pieces[j].toLowerCase());
 				}
-				boardSquares[i][j] = button;
-				boardPanel.add(button);
 			}
 		}
 
 		frame.setVisible(true);
+	}
+	public static void main (String[] args)
+	{
+		new ChessBoard();
 	}
 }
